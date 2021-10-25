bdbe43d33c33902f813c5e2ed8c3e592eb0b61e7
LaurianeSoares <lauriane_soares@yahoo.com.br>
2021-10-25 15:36:19 -0300

    Aulas de Pilhas e Filas

---
+++ Estrutura de dados em Java/Pilha/src/main/java/one/DigitalInovation/Pilha.java
@@ -0,0 +1,66 @@
+package one.DigitalInovation;
+
+public class Pilha {
+    private No refNoEntradaPilha;
+    public Pilha(){
+        this.refNoEntradaPilha = null;
+    }
+
+    public void push(No novoNo){
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setReNo(refAuxiliar);
+    }
+
+    public No pop(){
+        if (!this.isEmpty()){
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getReNo();
+            return noPoped;
+        }
+        return null;
+
+    }
+
+
+    public No top(){
+        return refNoEntradaPilha;
+    }
+
+
+    public boolean isEmpty(){
+  //      if(refNoEntradaPilha == null){
+  //          return true;
+  //      }
+  //      return false;
+    return refNoEntradaPilha == null ? true : false;
+
+
+    }
+
+    public No getRefNoEntradaPilha() {
+        return refNoEntradaPilha;
+    }
+
+    @Override
+
+    public String toString(){
+
+        String stringRetorno = "--------------\n";
+        stringRetorno += "   Pilha\n";
+        stringRetorno += "--------------\n";
+
+        No noAuxiliar = refNoEntradaPilha;
+        while(true){
+            if(noAuxiliar !=null) {
+                stringRetorno +="[No{dado="+noAuxiliar.getDado()+"}]\n";
+                noAuxiliar = noAuxiliar.getReNo();
+            }else{
+                break;
+            }
+        }
+        stringRetorno+="=============\n";
+        return stringRetorno;
+    }
+
+}
