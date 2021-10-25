592d813baede44cde762886945d0777627e2a340
LaurianeSoares <lauriane_soares@yahoo.com.br>
2021-10-25 15:36:19 -0300

    Aulas de Pilhas

---
+++ Estrutura de dados em Java/Pilha/src/main/java/one/DigitalInovation/Pilha.java
@@ -0,0 +1,40 @@
+package one.DigitalInovation;
+
+public class Pilha {
+    private No refNoEntradaPilha;
+    public Pilha(){
+        this.refNoEntradaPilha = null;
+    }
+
+    public void push(No novoNo){
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setReNo(refAuxiliar);
+    }
+
+    public No pop(){
+        if (this.isEmpty()){
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getReNo();
+            return noPoped;
+        }
+        return null;
+
+    }
+
+
+    public No top(){
+        return refNoEntradaPilha;
+    }
+
+
+    public boolean isEmpty(){
+  //      if(refNoEntradaPilha == null){
+  //          return true;
+  //      }
+  //      return false;
+    return refNoEntradaPilha == null ? true : false;
+
+
+    }
+}
